011f0f3463d9616525fcc7bb44655b32813ea195
Caio <167995557+cainhuu@users.noreply.github.com>
2024-09-13 14:11:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,50 @@
+let dados = [
+    {
+        titulo: "Banana",
+        descricao: "A banana é uma das frutas mais conhecidas e consumidas em todo o mundo. Contém grandes quantidades de açúcares e é rica em fibras e vitaminas.",
+        link: "https://pt.wikipedia.org/wiki/Banana",
+        tags: "banana amarela nanica"
+    },
+    {
+        titulo: "Melancia",
+        descricao: "Consumida em todo o mundo, a melancia é uma planta da família Cucurbitaceae. Tem importante valor social e econômico.",
+        link: "https://pt.wikipedia.org/wiki/Melancia",
+        tags: "melancia vermelha"
+    },
+    {
+        titulo: "Romã",
+        descricao: "A romã é uma baga redonda formada por uma polpa vermelha comestível e inúmeras sementes, que se abre espontaneamente quando atinge a maturação.",
+        link: "https://pt.wikipedia.org/wiki/Rom%C3%A3",
+        tags: "roma sementes"
+    },
+    {
+        titulo: "Maça",
+        descricao: "A maçã, fruto da macieira, auxilia no bom funcionamento intestinal, além de ter uma ação benéfica para o coração.",
+        link: "https://pt.wikipedia.org/wiki/Ma%C3%A7%C3%A3",
+        tags: "maca"
+    },
+    {
+        titulo: "Uva",
+        descricao: "A uva é o fruto da videira ou parreira, uma planta da família Vitaceae. É originária da Ásia e uma das frutas mais antigas utilizadas na alimentação humana.",
+        link: "https://pt.wikipedia.org/wiki/Uva",
+        tags: "uva roxa roxo"
+    },
+    {
+        titulo: "Ameixa",
+        descricao: "A ameixa é o fruto comestível da ameixeira. Pertencente à família das rosáceas, a ameixa de cor vermelha, amarela ou roxa escura é carnosa e suculenta.",
+        link: "https://pt.wikipedia.org/wiki/Ameixa",
+        tags: "ameixa"
+    },
+    {
+        titulo: "Damasco",
+        descricao: "O damasco é uma fruta redonda e amarela, parecida com um pêssego menor, mas com a acidez das ameixas roxas. É o fruto do damasqueiro, uma árvore originária da China e Sibéria que pode chegar a 7 metros de altura.",
+        link: "https://pt.wikipedia.org/wiki/Damasqueiro",
+        tags: "damasco laranja"
+    },
+    {
+        titulo: "Acerola",
+        descricao: "A acerola é uma fruta nativa da América Central, América do Sul e das ilhas do Caribe, conhecida também como cereja das Antilhas. É muito conhecida por causa do seu grande teor de ácido ascórbico, ou seja, vitamina, com apenas dois frutos as necessidades diárias de vitamina C são sanadas.",
+        link: "https://pt.wikipedia.org/wiki/Acerola",
+        tags: "açerola"
+    },
+];
